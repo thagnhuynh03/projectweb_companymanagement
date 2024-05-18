a84eba92927ed1de1b876370e56fbd1780a442fe
Kim Thang <thanghuynh.271203@gmail.com>
2024-05-18 20:40:32 +0700

    Create project with asp.net core (mvc)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace huynhkimthang_0145_Final_LTC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
