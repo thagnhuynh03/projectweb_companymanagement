b896d8344f2960185955843e1152d53655e80bc7
Kim Thang <thanghuynh.271203@gmail.com>
2024-05-22 01:21:16 +0700

    Create view and controller for CreateAnouncementPost feature

--- Controllers/MyPostController.cs
+++ Controllers/MyPostController.cs
@@ -8,10 +8,12 @@ namespace huynhkimthang_0145_Final_LTC_.Controllers
     public class MyPostController : Controller
     {
         private readonly CompanyManagementContext _context;
+        private readonly IWebHostEnvironment _environment;
 
-        public MyPostController(CompanyManagementContext context)
+        public MyPostController(CompanyManagementContext context, IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
         public async Task<IActionResult> Index()
         {
@@ -54,6 +56,52 @@ namespace huynhkimthang_0145_Final_LTC_.Controllers
                 .Where(s => s.Post.EmpId.Equals(idEmp))
                 .ToListAsync();
             return View(schedules);
+        }
+        [HttpGet]
+        public IActionResult CreateAnouncementPost()
+        {
+            ViewBag.Categories = _context.AnnouncementCategories.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateAnouncementPost(AnouncementViewModel model)
+        {
+            if (model.ThumbnailImg == null)
+            {
+                ModelState.AddModelError("ThumbnailImg", "The image file is required");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(model.ThumbnailImg!.FileName);
+
+            string imageFullPath = _environment.WebRootPath + "/Img/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                model.ThumbnailImg.CopyTo(stream);
+            }
+
+            Post post = new Post();
+            post.Title = model.Title;
+            post.Content = model.Content;
+            post.ThumbnailImg = newFileName;
+            post.CreatedDate = DateOnly.FromDateTime(DateTime.Now);
+            post.PostType = 1;
+            post.EmpId = HttpContext.Session.GetInt32("idEmp");
+
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+
+            Announcement announcement = new Announcement();
+            announcement.PostId = post.PostId;
+            announcement.AnnCateId = model.AnnCateId;
+            _context.Announcements.Add(announcement);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index", "MyPost");
         }
     }
 }
